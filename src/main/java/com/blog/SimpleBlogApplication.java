816f18183d50fca4b437d5a042c3edd2a2af9c4b
Bhupendra sambare <bhupendrasam1404@Bhupendras-Air.mshome.net>
2022-02-28 15:13:22 +0530

    Project Initialize

---
+++ src/main/java/com/blog/SimpleBlogApplication.java
@@ -0,0 +1,13 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlogApplication.class, args);
+	}
+
+}

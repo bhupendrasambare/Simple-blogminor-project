63803d2b34a965b7741802987855de9d4857a05f
Bhupendra sambare <bhupendrasam1404@Bhupendras-MacBook-Air.local>
2022-03-02 03:01:50 +0530

    Structure + template + resource + Index page PORT:8080

---
+++ src/main/java/com/blog/controler/pages.java
@@ -0,0 +1,13 @@
+package com.blog.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class pages {
+
+    @GetMapping({"","/","/index"})
+    public String index(){
+        return "index";
+    }
+}

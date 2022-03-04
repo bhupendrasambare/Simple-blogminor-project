9a9cf555971fff9a7dffae1c2ec645898cfeabac
Bhupendra sambare <bhupendrasam1404@Bhupendras-Air.mshome.net>
2022-03-04 11:11:26 +0530

    Initial pages added

--- src/main/java/com/blog/controler/pages.java
+++ src/main/java/com/blog/controler/pages.java
@@ -10,4 +10,19 @@ public class pages {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
 }

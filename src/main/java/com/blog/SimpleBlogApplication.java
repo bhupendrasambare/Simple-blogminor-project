963fda077af48e4234ee70f233be86ff6b21ff6b
bhupendrasam1404 <bhupendrasam1404@gmail.com>
2022-11-19 08:22:54 +0530

    tomcat added

--- src/main/java/com/blog/SimpleBlogApplication.java
+++ src/main/java/com/blog/SimpleBlogApplication.java
@@ -3,12 +3,13 @@ package com.blog;
 import com.blog.repository.usersRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = usersRepository.class)
-public class SimpleBlogApplication {
+public class SimpleBlogApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleBlogApplication.class, args);
